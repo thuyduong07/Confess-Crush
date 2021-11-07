6ac10a4772ce070856c38372eb26495e70d34574
thuyduong07 <93867822+thuyduong07@users.noreply.github.com>
2021-11-07 20:40:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'bởi vì m cute phô mai que  <3',
+    btnReply: 'gửi cho dương ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'h mik là ny nhau nha iu m :3.',
+    messDesc: 'tui thoát ế r mn ạ :)) :3.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
